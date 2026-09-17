40517da29d8d46e66bf93f2edb1fe088da9b5566
Abdimajid <abdimajid.m@icloud.com>
2026-09-17 15:48:40 +0200

    Har lagt till Join, Leave, Schedule samt ToString i Student

--- Student.cs
+++ Student.cs
@@ -7,4 +7,25 @@ public class Student // Klassen - självaste mallen.
     {
         Name = name;
     }
-}
+
+    public void Join(Course course) // Detta gör så att jag går med i en kurs.
+    {
+        course.Enroll(this); // återanvänder Enroll-logiken - this = dvs den här studenten.
+    }
+    public void Leave(Course course) // lämnar kursen
+    {
+        course.Remove(this); // Återanvänder Remove-logiken 
+    }
+
+    public void Schedule() // Skriver ut vilka kurser studenten går i.
+{
+        foreach (Course c in Courses) // Loopar igenom varje kurs i listan
+        {
+            Console.WriteLine(c.Name); // Skriver ut kursens namn
+        }
+    }
+
+    public override string ToString() // Studentens namn, istället för standardtexten C# annars visar
+    {
+        return Name;
+    }
