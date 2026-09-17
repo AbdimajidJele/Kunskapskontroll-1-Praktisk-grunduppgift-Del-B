771ea6862b06980c46a6aeb70889b976f4ca2cbd
Abdimajid <abdimajid.m@icloud.com>
2026-09-17 21:52:42 +0200

    Lagt till testkod i Program.cs som skapar kurs samt studenter, testar Enroll/Join

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Course mai26 = new Course ("MAI26", 2); // Skapar en kurs MAI26 med plats för 2 studenter
+Student abdimajid = new Student("Abdimajid");
+Student bertil = new Student("Bertil");
+Student ahmed = new Student("Ahmed");
+
+mai26.Enroll(abdimajid); // Anmäler Abdimajid via kursens metod
+bertil.Join(mai26); // Anmäler Bertil via studentens metod - testar att båda hållen fungerar
+mai26.Enroll(ahmed); // Kursen är nu full (2/2) -> Då ska den skriva "Kursen är tyvärr full"
+bertil.Join(mai26); // Bertil redan anmäld dvs det ska inte skapa dubblett
